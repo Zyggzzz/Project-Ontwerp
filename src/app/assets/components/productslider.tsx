132ebafc50fa6241da699726c5b1162983eebaae
Zyggzzz <dolislaegers@gmail.com>
2025-01-23 19:08:15 +0100

    LUCIDE

---
+++ src/app/assets/components/productslider.tsx
@@ -0,0 +1,11 @@
+export default function Productslider() {
+  return (
+    <div>
+      <button></button>
+      <div></div>
+      <div></div>
+      <div></div>
+      <button></button>
+    </div>
+  );
+}
